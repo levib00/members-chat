a4c31f94cd351baba4f3aa6fed3c484dab879480
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-08-16 22:16:01 -0400

    Add more routes to /users

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,25 @@
 const express = require('express');
 
 const router = express.Router();
-const usersController = require('../controllers/users-contoller');
+const usersController = require('../controllers/users-controller');
+const passphraseController = require('../controllers/passphrase-controller');
 
 /* GET users listing. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  res.redirect('/');
 });
 
 router.get('/sign-up', usersController.userCreateGet);
 
 router.post('/sign-up', usersController.userCreatePost);
+
+router.get('/create-member', passphraseController.makeMemberGet);
+
+router.post('/create-member', passphraseController.makeMemberPost);
+
+router.post('/log-in', usersController.userLogInGet);
+
+router.post('/log-in', usersController.userLogInPost);
 
 module.exports = router;
