0a35472f5bcdc4e3eb67e4fc4552f4a4df3927ec
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-08-20 21:35:43 -0400

    Fix update members operations. Change comments.

--- controllers/passphrase-controller.js
+++ controllers/passphrase-controller.js
@@ -7,77 +7,34 @@ const bcrypt = require('bcryptjs');
 const Passphrase = require('../models/passphrase');
 const User = require('../models/users');
 
-// Display snack create form on GET.
 exports.PassphraseGet = asyncHandler(async (req, res, next) => {
-  // Get all manufacturers and categories, which we can use for adding to our snack.
+  // Get passphrase enter form.
 
   res.render('passphrase', {
     title: 'passphrase',
   });
 });
 
-// Handle snack create on POST.
-exports.PassphrasePost = [
-
-  // Validate and sanitize fields.
-  body('passphrase', 'First name must not be empty.')
-    .isLength({ max: 32 })
-    .withMessage('password must be less than 32 characters long.')
-    .escape(),
-  body('passphraseConfirmation', 'First name must not be empty.')
-    .custom((value, { req }) => value === req.body.passphrase)
-    .withMessage('passwords must match.')
-    .escape(),
-
-  // Process request after validation and sanitization.
-
-  asyncHandler(async (req, res, next) => {
-    // Extract the validation errors from a request.
-    const errors = validationResult(req);
-
-    bcrypt.hash(req.body.passphrase, 10, async (err, hashedPassword) => {
-      // if err, do something
-      // otherwise, store hashedPassword in DB
-      if (err) {
-        //
-      } else {
-        const passphrase = new Passphrase({
-          passphrase: hashedPassword,
-          member_status: false,
-        });
-
-        if (!errors.isEmpty()) {
-          res.render('passphrase', {
-            title: 'passphrase',
-            errors: errors.array(),
-          });
-        } else {
-          // Data from form is valid. Save user.
-          res.redirect('/');
-          await passphrase.save();
-        }
-      }
-    });
-  }),
-];
-
 exports.makeMemberGet = asyncHandler(async (req, res, next) => {
-  // Get all manufacturers and categories, which we can use for adding to our snack.
-
+  // Get member password form.
   res.render('passphrase', {
-    title: 'Become a member!',
+    title: 'lightning',
   });
 });
 
 // Handle update member create on POST.
 exports.makeMemberPost = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne(req.user);
-  const passphrase = await Passphrase.findOne('64dc43252fbaa720a2df4965');
-  const match = await bcrypt.compare(req.body.passphrase, passphrase);
+  const user = await User.findById(req.user._id);
+  const passphrase = await Passphrase.findById('64dc43252fbaa720a2df4965');
+  const match = await bcrypt.compare(req.body.passphrase, passphrase.passphrase);
 
   if (!match) {
     res.render('passphrase', {
-      title: 'Become a member!',
+      title: 'lightning',
+      errors: 'Hint: look at the ',
+      href: 'https://en.wikipedia.org/wiki/Countersign_(military)',
+      link: ' wikipedia page for countersigns',
+      action: '/users/become-admin',
     });
   } else {
     const member = new User({
@@ -89,6 +46,7 @@ exports.makeMemberPost = asyncHandler(async (req, res, next) => {
       _id: user._id,
     });
 
-    User.findByIdAndUpdate(user._id, member, {});
+    await User.findByIdAndUpdate(user._id, member, {});
+    res.redirect('/');
   }
 });
