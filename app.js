a60ba1654f6d606dfdeb81a482b0ec58e53ba90e
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-08-20 21:51:54 -0400

    Add auth, sessions and routes.

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-/* eslint-disable import/no-unresolved */
 const path = require('path');
 const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/users');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const messagesRouter = require('./routes/messages');
 
 const mongoDb = process.env.DB_DRIVER_LINK;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -38,12 +59,17 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
 
 app.get('/log-out', (req, res, next) => {
   req.logout((err) => {
@@ -52,6 +78,11 @@ app.get('/log-out', (req, res, next) => {
     }
     return res.redirect('/');
   });
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
 });
 
 // catch 404 and forward to error handler
