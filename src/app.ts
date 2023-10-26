fd8262cb96a930fbeb756b236846063f457beac0
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-10-26 18:04:15 -0400

    Chnage app to typescript

---
+++ src/app.ts
@@ -0,0 +1,106 @@
+const path = require('path');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/users');
+
+const indexRouter = require('../routes/index');
+const usersRouter = require('../routes/users');
+const messagesRouter = require('../routes/messages');
+
+const mongoDb = process.env.DB_DRIVER_LINK;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+passport.use(
+  new LocalStrategy(async (username: string, password: string, done: (arg0: null | unknown, arg1?: boolean | undefined, arg2?: { message: string; } | undefined) => any) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user: { id: string; }, done: (arg0: null, arg1: any) => void) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id: string, done: (arg0: unknown, arg1?: undefined) => void) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
+
+app.get('/log-out', (req: { logout: (arg0: (err: any) => any) => void; }, res: { redirect: (arg0: string) => any; }, next: (arg0: any) => any) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    return res.redirect('/');
+  });
+});
+
+app.use((req: { user: any; }, res: { locals: { currentUser: any; }; }, next: () => void) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use((req: any, res: any, next: (arg0: any) => void) => {
+  next(createError(404));
+});
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((err: { message: any; status: any; }, req: { app: { get: (arg0: string) => string; }; }, res: { locals: { message: any; error: any; }; status: (arg0: any) => void; render: (arg0: string) => void; }, next: any) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
