acc84417bbc3d3049e4242cd992d84448d3c979f
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-11-17 05:50:12 -0500

    Add functions for editing/deleting messages.

--- client/src/components/message.tsx
+++ client/src/components/message.tsx
@@ -1,32 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateCreateDeleteMessage, validateMessageEdit } from "../utility-functions/post-fetch";
+import { useNavigate, useParams } from "react-router-dom";
+import { IErrorObject } from "../App";
 
-interface MessageInfo {
+
+interface IMessageInfo {
+  username: {
+    _id: any, 
+    firstName: string,
+    lastName: string,
+    username: string
+  },
+  timestamp: string,
+  content: string,
+  _id: any
+};
+
+interface IUserObject {
+  _id: any,
   username: string,
-  timeStamp: string,
-  message: string
+  isAdmin: boolean,
+}
+
+interface IMessagesProps {
+  currentUser: IUserObject,
+  messageInfo: IMessageInfo,
+  setError: React.Dispatch<React.SetStateAction<IErrorObject>>,
 };
 
-interface MessagesProps {
-  messageInfo: MessageInfo,
-};
+function Message(props: IMessagesProps) {
+  const {messageInfo, currentUser, setError} = props;
+  const {username, timestamp, content, _id} = messageInfo;
 
-function Message(props: MessagesProps) {
-  const {messageInfo} = props;
-  const {username, timeStamp, message} = messageInfo;
+  const [messageInput, setMessageInput] = useState(content)
+  const [isEditing, setIsEditing] = useState(false)
+  const [validationError, setValidationError] = useState('')
 
-  const date = new Date(timeStamp);
+  const date = new Date(timestamp);
+  const navigate = useNavigate()
+
+  const deleteMessage = async() => {
+    try {
+      const response = await fetch(`http://localhost:3000/messages/delete/${_id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+        //@ts-ignore
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:5173',
+          Authorization: (() => {
+            const token = localStorage.getItem('jwt');
+            if (token) {
+              return 'Bearer ' + token
+            }
+            return null
+          })()
+        },      
+        mode: 'cors'
+      })
+      validateCreateDeleteMessage(response, navigate, setValidationError)
+    } catch (error: any) {
+      setError(error) // Redirect to error page if there is a non-validation error.
+    }
+  }
+
+  const sendEdit = async() => {
+    try {
+      const response = await fetch(`http://localhost:3000/messages/edit/${_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({content: messageInput}),
+        credentials: 'include',
+        //@ts-ignore
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:5173',
+          Authorization: (() => {
+            const token = localStorage.getItem('jwt');
+            if (token) {
+              return 'Bearer ' + token
+            }
+            return null
+          })()
+        },      
+        mode: 'cors'
+      })
+      validateMessageEdit(response, navigate, setValidationError)
+    } catch (error: any) {
+      setError(error) // Redirect to error page if there is a non-validation error.
+    }
+  }
 
   return (
     <>
       <div>
-        {username}
+        <div>
+          {username.username}
+        </div>
+        <div>
+          {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}
+        </div>
+        <div>
+          {isEditing ? <input type='text' onChange={(e) => setMessageInput(e.target.value)} value={messageInput}/> : content}
+        </div>
       </div>
-      <div>
-        {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}
-      </div>
-      <div>
-        {message}
-      </div>
+      {
+        ((currentUser._id === username._id) || currentUser.isAdmin) &&
+        <div>
+          {((currentUser._id === username._id) && (isEditing ? <button onClick={() => setIsEditing(!isEditing)}>Cancel</button> : <button onClick={() => setIsEditing(!isEditing)}>Edit</button>))}
+          {isEditing ? <button onClick={sendEdit}>save</button> : <button onClick={deleteMessage}>Delete</button>}
+        </div>
+      }
+      <p>{validationError}</p>
     </>
   )
 };
