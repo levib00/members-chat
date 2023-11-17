36cfb0723d1c2287e94f1457d4b77c957d68e6fc
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-11-17 05:41:25 -0500

    Add different homepage for if user is logged in.

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,15 +1,36 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-const Home = () => {
+export interface IHomeProps {
+  hasAuth: boolean
+};
 
+const Home = (props: IHomeProps) => {
+
+  const { hasAuth } = props
   return (
     <div>
-      <h2>
-        Join to start chatting now!
-      </h2>
-      <Link>
-        <button>Sign up now!</button>
-      </Link>
+      {
+       !hasAuth ? 
+       <>
+        <h2>
+          Join to start chatting now!
+        </h2>
+        <Link to='/sign-up'>
+          <button>Sign up now!</button>
+        </Link>
+      </>
+      :
+      <>
+        <h2>
+          Start chatting now!
+        </h2>
+        <Link to='/chatrooms'>
+          <button>See servers!</button>
+        </Link>
+      </>
+      }
+      
     </div>
   )
 }
