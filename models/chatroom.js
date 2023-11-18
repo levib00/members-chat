7923fc99214826fe5ee31408835509e4d6108a93
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-11-18 00:55:06 -0500

    Add createdBy field. Change validation checks.

--- models/chatroom.js
+++ models/chatroom.js
@@ -2,18 +2,21 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const AdminSchema = new Schema({
+const ChatroomSchema = new Schema({
 
   roomName: {
-    type: String, required: true,
+    type: String, required: true, minLength: 3, maxLength: 64,
   },
   isPublic: {
     type: Boolean, required: true,
   },
   password: {
-    type: String, required: true,
+    type: String, minLength: 8, maxLength: 64,
+  },
+  createdBy: {
+    type: Schema.ObjectId, ref: 'user', required: true,
   },
 });
 
 // Export model.
-module.exports = mongoose.model('admin', AdminSchema);
+module.exports = mongoose.model('chatroom', ChatroomSchema);
