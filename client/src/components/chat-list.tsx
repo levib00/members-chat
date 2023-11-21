aadaf4e5c4304ea5eaae87510eca4cfd4f653d7e
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-11-21 04:04:36 -0500

    destructure prop and change card props for admins.

--- client/src/components/chat-list.tsx
+++ client/src/components/chat-list.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import ChatroomCard from "./chatroom-card"
 import { getFetcher } from "../utility-functions/fetcher"
 import useSWR from "swr"
@@ -18,6 +18,7 @@ interface IChatroomListProps {
 }
 
 const ChatroomList = (props: IChatroomListProps) => {
+  const { setError } = props
 
   const {data: user,  error: userError} = useSWR(`http://localhost:3000/users/user`, getFetcher)
 
@@ -26,7 +27,7 @@ const ChatroomList = (props: IChatroomListProps) => {
   return (
     <>
       <Link to='/chatrooms/new'><button>Create a new chatroom</button></Link>
-      {user && chatrooms?.map((chatroom: IChatroomInfo) => <ChatroomCard key={uuid()} chatroomInfo={chatroom} setError={props.setError} hasUser={user.chatrooms.includes(chatroom._id)} />)}
+      {user && chatrooms?.map((chatroom: IChatroomInfo) => <ChatroomCard key={uuid()} chatroomInfo={chatroom} setError={setError} hasUser={(user.chatrooms.includes(chatroom._id) || user.isAdmin)} />)}
     </>
   )
 }
