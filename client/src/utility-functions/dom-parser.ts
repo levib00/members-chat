81549b99574da3e44edd80836e96cdaaa5440ccf
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-11-27 05:33:48 -0500

    Set default return to fix ts error.

--- client/src/utility-functions/dom-parser.ts
+++ client/src/utility-functions/dom-parser.ts
@@ -1,6 +1,6 @@
 const parseDom = (str: string) => {  // Parses special characters from html code to unicode characters.
   const doc = new DOMParser().parseFromString(str, "text/html");
-  return doc.documentElement.textContent;
+  return doc.documentElement.textContent || '';
 }
 
 export default parseDom
