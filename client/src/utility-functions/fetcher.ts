a1ea944eecaef580eab9c1529915e8ab3ca30949
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-12-03 04:44:20 -0500

    Refactor.

--- client/src/utility-functions/fetcher.ts
+++ client/src/utility-functions/fetcher.ts
@@ -1,25 +1,26 @@
 const getFetcher = async (url: string) => {
-  const data = await fetch(url, { 
+  const data = await fetch(url, {
     method: 'GET',
-    //@ts-ignore
+    // @ts-ignore
     headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    Authorization: (() => {
-      const token = localStorage.getItem('jwt');
-      if (token) {
-        return 'Bearer ' + token
-      }
-      return null
-    })()
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: (() => {
+        const token = localStorage.getItem('jwt');
+        if (token) {
+          return `Bearer ${token}`;
+        }
+        return null;
+      })(),
     },
     // 'Access-Control-Allow-Origin': '*',
-    mode: 'cors'
-  })
+    mode: 'cors',
+  });
   if (data.status === 200) {
-    return data.json()
+    return data.json();
   }
-  throw new Error(`${data.status}, something went wrong`)
-}
+  throw new Error(`${data.status}, something went wrong`);
+};
 
-export{ getFetcher }
+// eslint-disable-next-line import/prefer-default-export
+export { getFetcher };
