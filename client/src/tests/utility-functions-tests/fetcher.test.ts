1147b91c6d89fcabebacf12ca9973ff48c84a83f
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-12-13 05:04:41 -0500

    Update tests for updated app.

--- client/src/tests/utility-functions-tests/fetcher.test.ts
+++ client/src/tests/utility-functions-tests/fetcher.test.ts
@@ -1,20 +1,18 @@
-/* *
-*@jest-environment node
-*/
 // @ts-nocheck
-import * as utils from "../../../client/src/utility-functions/fetcher";
+import * as utils from '../../utility-functions/fetcher';
 
-describe("Fetchers are called right", () => {
-
-  test('getFetcher is called with correct params', async() => {
-    global.fetch = jest.fn(() =>
-    Promise.resolve(
-      {status: 200, json: jest.fn(() => 'works')})
-    );
+describe('Fetchers are called right', () => {
+  test('getFetcher is called with correct params', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({ status: 200, json: jest.fn(() => 'works') }));
 
     const getResponse = await utils.getFetcher('localhost:3000');
 
-    expect(getResponse).toBe('works')
-    
+    expect(getResponse).toBe('works');
   });
-})
+
+  test('getFetcher is called with correct params', async () => {
+    global.fetch = jest.fn(() => Promise.reject(new Error('error')));
+
+    expect(() => utils.getFetcher('localhost:3000')).rejects.toThrowError();
+  });
+});
--- client/src/tests/utility-functions-tests/post-fetch.test.ts
+++ client/src/tests/utility-functions-tests/post-fetch.test.ts
@@ -4,7 +4,7 @@
 // @ts-nocheck
 import * as utils from '../../utility-functions/post-fetch';
 
-describe('Post fetch', () => {
+describe('Post fetch Create chat.', () => {
   test('Create chat navigates to chatrooms on good response', async () => {
     global.fetch = jest.fn(() => Promise.resolve(
       { status: 200, json: jest.fn(() => 'works') },
@@ -12,7 +12,7 @@ describe('Post fetch', () => {
 
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
     const mockBody = { one: '', two: '', three: '' };
@@ -22,23 +22,24 @@ describe('Post fetch', () => {
       mockBody,
       e,
       utils.validateCreateChat,
-      setError,
       setValidationError,
       navigate,
       null,
+      sendMessage,
     );
 
     expect(navigate).toBeCalledWith('/chatrooms');
   });
 
-  test('Sets wrong username or password message on 401 error. ', async () => {
-    global.fetch = jest.fn(() => Promise.resolve(
-      { status: 401, json: jest.fn(() => 'works') },
-    ));
+  test('Adds errors sent from the server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
 
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
     const mockBody = { one: '', two: '', three: '' };
@@ -48,40 +49,10 @@ describe('Post fetch', () => {
       mockBody,
       e,
       utils.validateCreateChat,
-      setError,
       setValidationError,
       navigate,
       null,
-    );
-
-    expect(setValidationError).toBeCalledWith('Chat name or password are invalid.');
-  });
-
-  test('Adds something went wrong on unexpected errors', async () => {
-    global.fetch = jest.fn(() => Promise.resolve(
-      { status: 403, json: jest.fn(() => 'works') },
-    ));
-
-    global.fetch = jest.fn(() => Promise.resolve(
-      { status: 403, json: jest.fn(() => 'works') },
-    ));
-
-    const url = 'localhost:8000';
-    const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
-    const setValidationError = jest.fn();
-    const navigate = jest.fn();
-    const mockBody = { one: '', two: '', three: '' };
-
-    await utils.submitPost(
-      url,
-      mockBody,
-      e,
-      utils.validateCreateChat,
-      setError,
-      setValidationError,
-      navigate,
-      null,
+      sendMessage,
     );
 
     expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
@@ -96,7 +67,7 @@ describe('Post fetch sign Up', () => {
 
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
     const mockBody = { one: '', two: '', three: '' };
@@ -106,10 +77,10 @@ describe('Post fetch sign Up', () => {
       mockBody,
       e,
       utils.validateSignUp,
-      setError,
       setValidationError,
       navigate,
       null,
+      sendMessage,
     );
 
     expect(navigate).toBeCalledWith('/');
@@ -122,7 +93,7 @@ describe('Post fetch sign Up', () => {
 
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
     const mockBody = { one: '', two: '', three: '' };
@@ -132,27 +103,24 @@ describe('Post fetch sign Up', () => {
       mockBody,
       e,
       utils.validateSignUp,
-      setError,
       setValidationError,
       navigate,
       null,
+      sendMessage,
     );
 
     expect(setValidationError).toBeCalledWith('Username or password are invalid.');
   });
 
-  test('Adds something went wrong on unexpected errors', async () => {
-    global.fetch = jest.fn(() => Promise.resolve(
-      { status: 403, json: jest.fn(() => 'works') },
-    ));
-
-    global.fetch = jest.fn(() => Promise.resolve(
-      { status: 403, json: jest.fn(() => 'works') },
-    ));
+  test('Adds errors sent from server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
 
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
     const mockBody = { one: '', two: '', three: '' };
@@ -162,17 +130,17 @@ describe('Post fetch sign Up', () => {
       mockBody,
       e,
       utils.validateSignUp,
-      setError,
       setValidationError,
       navigate,
       null,
+      sendMessage,
     );
 
     expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
   });
 });
 
-describe('Post fetch log in', () => {
+describe('Post fetch log in', () => { // TODO: add happy path for this and sign up.
   test('Sets wrong username or password message on 401 error. ', async () => {
     global.fetch = jest.fn(() => Promise.resolve(
       { status: 401, json: jest.fn(() => 'works') },
@@ -187,7 +155,7 @@ describe('Post fetch log in', () => {
 
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
     const setHasAuth = jest.fn();
@@ -198,18 +166,61 @@ describe('Post fetch log in', () => {
       mockBody,
       e,
       utils.validateLogIn,
-      setError,
       setValidationError,
       navigate,
       setHasAuth,
+      sendMessage,
     );
 
     expect(setValidationError).toBeCalledWith('Wrong username or password.');
   });
 
-  test('Adds something went wrong on unexpected errors', async () => {
+  test('Adds error from server errors', async () => {
+    const localStorageMock = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      clear: jest.fn(),
+    };
+
+    global.localStorage = localStorageMock as unknown as Storage;
+
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({
+        error: 'Something went wrong. Please try again.',
+      })),
+    }));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const setHasAuth = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateLogIn,
+      setValidationError,
+      navigate,
+      setHasAuth,
+      sendMessage,
+    );
+
+    expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
+  });
+});
+
+describe('Post fetch Make Admin.', () => {
+  test('Happy path.', async () => {
     global.fetch = jest.fn(() => Promise.resolve(
-      { status: 403, json: jest.fn(() => 'works') },
+      {
+        status: 200,
+        json: jest.fn(() => ({ token: 'token' })),
+      },
     ));
 
     const localStorageMock = {
@@ -219,27 +230,299 @@ describe('Post fetch log in', () => {
     };
     global.localStorage = localStorageMock as unknown as Storage;
 
-    global.fetch = jest.fn(() => Promise.resolve(
-      { status: 403, json: jest.fn(() => 'works') },
-    ));
-
     const url = 'localhost:8000';
     const e = { preventDefault: jest.fn() };
-    const setError = jest.fn();
+    const sendMessage = jest.fn();
     const setValidationError = jest.fn();
     const navigate = jest.fn();
-    const setHasAuth = jest.fn();
     const mockBody = { one: '', two: '', three: '' };
 
     await utils.submitPost(
       url,
       mockBody,
       e,
-      utils.validateLogIn,
-      setError,
+      utils.validateMakeAdmin,
       setValidationError,
       navigate,
-      setHasAuth,
+      null,
+      sendMessage,
+    );
+
+    expect(localStorageMock.setItem).toBeCalledWith('jwt', 'token');
+  });
+
+  test('Adds errors sent from the server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateMakeAdmin,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
+  });
+});
+
+describe('Post fetch Leave Chatroom.', () => {
+  test('Happy path.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve(
+      {
+        status: 200,
+        json: jest.fn(() => ({ token: 'token' })),
+      },
+    ));
+
+    const localStorageMock = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      clear: jest.fn(),
+    };
+    global.localStorage = localStorageMock as unknown as Storage;
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateLeaveChatroom,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    expect(localStorageMock.setItem).toBeCalledWith('jwt', 'token');
+  });
+
+  test('Adds errors sent from the server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateLeaveChatroom,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
+  });
+});
+
+describe('Post fetch Join Chatroom.', () => {
+  test('Happy path.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve(
+      {
+        status: 200,
+        json: jest.fn(() => ({ token: 'token' })),
+      },
+    ));
+
+    const localStorageMock = {
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+      clear: jest.fn(),
+    };
+    global.localStorage = localStorageMock as unknown as Storage;
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateJoinChatroom,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    expect(localStorageMock.setItem).toBeCalledWith('jwt', 'token');
+  });
+
+  test('Adds errors sent from the server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateJoinChatroom,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
+  });
+});
+
+describe('Post fetch Create/Delete message', () => {
+  test('Happy path.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve(
+      {
+        status: 200,
+        json: jest.fn(() => ({ message: 'message' })),
+      },
+    ));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateCreateDeleteMessage,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    const jsonResponse = { message: 'message' };
+
+    expect(sendMessage).toBeCalledWith(JSON.stringify(jsonResponse));
+  });
+
+  test('Adds errors sent from the server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateCreateDeleteMessage,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
+  });
+});
+
+describe('Post fetch edit Message.', () => {
+  test('Happy path.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve(
+      {
+        status: 200,
+        json: jest.fn(() => ({ message: 'message' })),
+      },
+    ));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateMessageEdit,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
+    );
+
+    const jsonResponse = { message: 'message' };
+
+    expect(sendMessage).toBeCalledWith(JSON.stringify(jsonResponse));
+  });
+
+  test('Adds errors sent from the server.', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      status: 403,
+      json: jest.fn(() => ({ error: 'Something went wrong. Please try again.' })),
+    }));
+
+    const url = 'localhost:8000';
+    const e = { preventDefault: jest.fn() };
+    const sendMessage = jest.fn();
+    const setValidationError = jest.fn();
+    const navigate = jest.fn();
+    const mockBody = { one: '', two: '', three: '' };
+
+    await utils.submitPost(
+      url,
+      mockBody,
+      e,
+      utils.validateMessageEdit,
+      setValidationError,
+      navigate,
+      null,
+      sendMessage,
     );
 
     expect(setValidationError).toBeCalledWith('Something went wrong. Please try again.');
