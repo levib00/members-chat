aa4a8e590fca304e9a923a873001b4b0f294aa04
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-12-14 02:47:37 -0500

    Add box to make a user an admin, and a redirect. If user is not logged in they get redirected to log in page.

--- client/src/components/chat-list.tsx
+++ client/src/components/chat-list.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import { v4 as uuid } from 'uuid';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getFetcher } from '../utility-functions/fetcher';
 import ChatroomCard from './chatroom-card';
 
@@ -13,12 +13,30 @@ interface IChatroomInfo {
 }
 
 const ChatroomList = () => {
-  const { data: user } = useSWR('http://localhost:3000/users/user', getFetcher);
+  const [adminInput, setAdminInput] = useState('');
+  const navigate = useNavigate();
+
+  const { data: user, error: userError } = useSWR('http://localhost:3000/users/user', getFetcher);
 
   const { data: chatrooms } = useSWR('http://localhost:3000/chatrooms/', getFetcher);
 
+  useEffect(() => {
+    if (userError) {
+      navigate('/log-in');
+    }
+  }, [userError]);
+
   return (
     <>
+    <div>
+    {user?.isAdmin && <>
+      <p>Enter a users name to make them an admin:</p>
+       <div>
+        <input type='text' onChange={(e) => setAdminInput(e.target.value)} value={adminInput}/>
+        <button>Submit</button>
+      </div>
+      </>}
+    </div>
       <Link to='/chatrooms/new'><button>Create a new chatroom</button></Link>
       {user && chatrooms?.map(
         (chatroom: IChatroomInfo) => <ChatroomCard key={uuid()}
