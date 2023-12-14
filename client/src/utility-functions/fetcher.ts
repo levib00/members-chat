d94c5b0889695770b94b6658efcf183a0eeb4f9e
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-12-14 01:57:37 -0500

    Add try catch block.

--- client/src/utility-functions/fetcher.ts
+++ client/src/utility-functions/fetcher.ts
@@ -1,25 +1,27 @@
 const getFetcher = async (url: string) => {
-  const data = await fetch(url, {
-    method: 'GET',
-    // @ts-ignore
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: (() => {
-        const token = localStorage.getItem('jwt');
-        if (token) {
-          return `Bearer ${token}`;
-        }
-        return null;
-      })(),
-    },
-    // 'Access-Control-Allow-Origin': '*',
-    mode: 'cors',
-  });
-  if (data.status === 200) {
-    return data.json();
+  try {
+    const data = await fetch(url, {
+      method: 'GET',
+      // @ts-ignore
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: (() => {
+          const token = localStorage.getItem('jwt');
+          if (token) {
+            return `Bearer ${token}`;
+          }
+          return null;
+        })(),
+      },
+      // 'Access-Control-Allow-Origin': '*',
+      mode: 'cors',
+    });
+
+    return await data.json();
+  } catch (error: any) {
+    throw new Error(error);
   }
-  throw new Error(`${data.status}, something went wrong`);
 };
 
 // eslint-disable-next-line import/prefer-default-export
