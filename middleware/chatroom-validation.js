6fbcb99305d2042ffff5f9b4fdac03e478fa9765
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-12-14 02:06:44 -0500

    Fix bug caused by sending response in middleware.

--- middleware/chatroom-validation.js
+++ middleware/chatroom-validation.js
@@ -17,10 +17,7 @@ module.exports = [
     .escape(),
 
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+    req.ValidationErrors = validationResult(req);
     return next();
   },
 ];
