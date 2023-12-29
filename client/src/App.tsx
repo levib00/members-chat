4ccd3e6c1c49e57abe76fc3e59f20521bf86501f
Levi B <bekurkes@DESKTOP-B2V4ILA>
2023-12-29 05:38:46 -0500

    Add footer and change path of style import.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/nav.js';
 import Home from './components/home.js';
 import SignUp from './components/sign-up.js';
-import './App.css';
+import './styles/App.scss';
 import ChatroomList from './components/chat-list.js';
 import Chatroom from './components/chatroom.js';
 import CreateChat from './components/create-chat.js';
 import LogIn from './components/log-in.js';
 import Error from './components/error.js';
+import Footer from './components/footer.js';
 
 export interface IErrorObject {
   status: number,
@@ -24,15 +25,16 @@ const App = () => {
       <BrowserRouter>
         <NavBar hasAuth={hasAuth} setHasAuth={setHasAuth} />
         <Routes>
-          <Route path='/' element={<Home hasAuth={hasAuth} />} />
-          <Route path='/chatrooms' element={<ChatroomList />} />
-          <Route path='/chatrooms/:chatroomId' element={<Chatroom setError={setError} />} />
-          <Route path='/chatrooms/new' element={<CreateChat isAnEdit={false} chatroom={null}/>} />
-          <Route path='/log-in' element={<LogIn hasAuth={hasAuth} setHasAuth={setHasAuth} />} />
-          <Route path='/sign-up' element={<SignUp hasAuth={hasAuth} /> } />
-          <Route path='/error' element={<Error error={error} />} />
-          <Route path='*' element={<Error error={error} />} />
+            <Route path='/' element={<Home hasAuth={hasAuth} />} />
+            <Route path='/chatrooms' element={<ChatroomList />} />
+            <Route path='/chatrooms/:chatroomId' element={<Chatroom setError={setError} />} />
+            <Route path='/chatrooms/new' element={<CreateChat isAnEdit={false} chatroom={null}/>} />
+            <Route path='/log-in' element={<LogIn hasAuth={hasAuth} setHasAuth={setHasAuth} />} />
+            <Route path='/sign-up' element={<SignUp hasAuth={hasAuth} /> } />
+            <Route path='/error' element={<Error error={error} />} />
+            <Route path='*' element={<Error error={error} />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
