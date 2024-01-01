623e776e7a16094bb41c4644b4b03a69dc75c63c
Levi B <levib2000@gmail.com>
2023-12-31 22:10:18 -0500

    Linting

--- jest.config.ts
+++ jest.config.ts
@@ -2,19 +2,19 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-      "^.+\\.tsx?$": "ts-jest" 
+    '^.+\\.tsx?$': 'ts-jest',
   // process `*.tsx` files with `ts-jest`
   },
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  modulePathIgnorePatterns: ["dist/"],
+  coverageDirectory: 'coverage',
+  modulePathIgnorePatterns: ['dist/'],
   moduleNameMapper: {
-      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
   },
   globals: {
     fetch: global.fetch,
-    localStorage: global.localStorage
+    localStorage: global.localStorage,
   },
-  setupFiles: ["jest-localstorage-mock"]
-}
+  setupFiles: ['jest-localstorage-mock'],
+};
