896d2c053d2ddf0ebafae6f553ab346ca8e712b6
Levi B <levib2000@gmail.com>
2024-01-03 17:31:15 -0500

    Update dist build

--- client/dist/src/utility-functions/dom-parser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const parseDom = (str) => {
-    // Parses special characters from html code to unicode characters.
-    const doc = new DOMParser().parseFromString(str, 'text/html');
-    return doc.documentElement.textContent || '';
-};
-exports.default = parseDom;
-//# sourceMappingURL=dom-parser.js.map
