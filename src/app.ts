cf0a5a8858ee3508bfde4409bf3c883fede86c69
Levi B <levib2000@gmail.com>
2024-01-04 18:40:45 -0500

    Linting and cors changes

--- src/app.ts
+++ src/app.ts
@@ -1,113 +1,128 @@
-  const path = require('path');
-  const createError = require('http-errors');
-  const express = require('express');
-  const cookieParser = require('cookie-parser');
-  const logger = require('morgan');
-  const session = require('express-session');
-  const mongoose = require('mongoose');
-  require('dotenv').config();
-  const passport = require('passport');
-  const bcrypt = require('bcryptjs');
-  const LocalStrategy = require('passport-local').Strategy;
-  const cors = require('cors');
+const path = require('path');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const LocalStrategy = require('passport-local').Strategy;
+const cors = require('cors');
 
-  const User = require('../../models/users');
+const User = require('../../models/users');
 
-  const usersRouter = require('../../routes/users');
-  const messagesRouter = require('../../routes/messages');
-  const chatroomsRouter = require('../../routes/chatrooms');
+const usersRouter = require('../../routes/users');
+const messagesRouter = require('../../routes/messages');
+const chatroomsRouter = require('../../routes/chatrooms');
 
-  const mongoDb = process.env.DB_DRIVER_LINK;
-  mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'mongo connection error'));
+const mongoDb = process.env.DB_DRIVER_LINK;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
 
-  passport.use(
-    new LocalStrategy(async (username: string, password: string, done: (arg0: null | unknown, arg1?: boolean | undefined, arg2?: { message: string; } | undefined) => any) => {
-      try {
-        const user = await User.findOne({ username });
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username' });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: 'Incorrect password' });
-        }
-        return done(null, user);
-      } catch (err) {
-        return done(err);
-      }
-    }),
-  );
-
-  passport.serializeUser((user: { id: string; }, done: (arg0: null, arg1: any) => void) => {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(async (id: string, done: (arg0: unknown, arg1?: undefined) => void) => {
+passport.use(
+  new LocalStrategy(async (
+    username: string,
+    password: string,
+    done: (
+      arg0: null | unknown,
+      arg1?: boolean | undefined,
+      arg2?: { message: string; } | undefined
+    ) => any,
+  ) => {
     try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  });
-
-  const whitelist: Array<string> = ['http://localhost:5173'];
-
-  interface ICorsOptions {
-    origin: any,
-    credentials: boolean,
-  };
-
-  const corsOptions: ICorsOptions = {
-    origin(origin: any, callback: any) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
       }
-    },
-    credentials: true,
-  };
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
 
-  const app: any = express();
+passport.serializeUser((user: { id: string; }, done: (arg0: null, arg1: any) => void) => {
+  done(null, user.id);
+});
 
-  app.use(logger('dev'));
-  app.use(express.json());
+passport.deserializeUser(async (id: string, done: (arg0: unknown, arg1?: undefined) => void) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
-  app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(cors(corsOptions));
+const whitelist: Array<string> = ['https://levib00.github.io'];
 
-  app.use('/users', usersRouter);
-  app.use('/messages', messagesRouter);
-  app.use('/chatrooms', chatroomsRouter);
+interface ICorsOptions {
+  origin: any,
+  credentials: boolean,
+}
 
-  app.use((req: { user: any; }, res: { locals: { currentUser: any; }; }, next: () => void) => {
-    res.locals.currentUser = req.user;
-    next();
-  });
+const corsOptions: ICorsOptions = {
+  origin(origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
 
-  // catch 404 and forward to error handler
-  app.use((req: any, res: any, next: (arg0: any) => void) => {
-    next(createError(404));
-  });
+const app: any = express();
 
-  // error handler
-  // eslint-disable-next-line no-unused-vars
-  app.use((err: { message: any; status: any; }, req: { app: { get: (arg0: string) => string; }; }, res: { locals: { message: any; error: any; }; status: (arg0: any) => void; render: (arg0: string) => void; }, next: any) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(logger('dev'));
+app.use(express.json());
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
-  module.exports = app;
+app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
+app.use('/chatrooms', chatroomsRouter);
+
+app.use((req: { user: any; }, res: { locals: { currentUser: any; }; }, next: () => void) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use((req: any, res: any, next: (arg0: any) => void) => {
+  next(createError(404));
+});
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((
+  err: { message: any; status: any; },
+  req: { app: { get: (arg0: string) => string; }; },
+  res: {
+    locals: { message: any; error: any; };
+    status: (arg0: any) => void;
+    render: (arg0: string) => void; },
+) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
