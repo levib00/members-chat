dff9c77cd2b18e61a703cd25616782367631a756
Levi B <levib2000@gmail.com>
2024-01-05 00:53:19 -0500

    add console log for debugging deploy

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ const mongoDb = process.env.DB_DRIVER_LINK;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
+console.log('env', process.env);
 
 passport.use(
   new LocalStrategy(async (
