652d3316df0929e919f6c5952610a19a17204ad6
Levi B <levib2000@gmail.com>
2024-01-06 07:08:01 -0500

    fix error handling on sign in

--- client/src/utility-functions/post-fetch.ts
+++ client/src/utility-functions/post-fetch.ts
@@ -119,7 +119,7 @@ export const validateLogIn = async (
     return response.status;
   }
   const errorResponse = await response.json();
-  setValidationError(errorResponse.error); // Set validation error from response
+  // setValidationError(errorResponse.error); // Set validation error from response
   return errorResponse;
 };
 
@@ -198,7 +198,8 @@ export const submitPost = async (
       sendMessage,
     );
   } catch (error: any) {
-    setValidationError(error); // Set validation error in case of an error
+    console.error(error);
+    setValidationError('Something went wrong.'); // Set error in case of an error
     return error;
   }
 };
